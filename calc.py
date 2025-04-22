357dbf6f5db1f207ff49ffcb663813d3dccff612
HananBlc9 <hanan.balucci@gmail.com>
2025-04-22 09:37:59 +0400

    added factorial function fact()

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
+
+
 print("Addition: 2 + 2 = ",2+2)
 print("Subtraction: 2 - 2 = ",2-2)
 print("Multiplication: 2 * 2 = ",2*2)
@@ -7,3 +9,10 @@ def power2(x):
     return x**2
 
 print("Power 2: 2**2 = ", power2(2))
+
+def fact(x):
+    if x <= 0:
+        return 1
+    else:
+        return x*fact(x-1)
+print("Factorial 2: 2*1 = ", fact(2))
