90e86af6a56a67a8828d904a7f675a374a14c3ca
HananBlc9 <hanan.balucci@gmail.com>
2025-04-22 09:26:57 +0400

    added power2 function

--- calc.py
+++ calc.py
@@ -2,3 +2,8 @@ print("Addition: 2 + 2 = ",2+2)
 print("Subtraction: 2 - 2 = ",2-2)
 print("Multiplication: 2 * 2 = ",2*2)
 print("Division: 2 / 2 = ",2/2)
+
+def power2(x):
+    return x**2
+
+print("Power 2: 2**2 = ", power2(2))
