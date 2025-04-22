089085501593be85d5753fe90a93546ed15f2583
HananBlc9 <hanan.balucci@gmail.com>
2025-04-22 09:12:53 +0400

    added previous python files

---
+++ D1_14_4_2025.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print("*****")
+
+x = input("Enter an integer: ")
+for i in range(int(x)*2):
+    if i%2 == 1:
+        print("*"*i)
+
+y = int(input("Enter an integer: "))
+q = y
+for i in range(y*2):
+    if i%2 == 1:
+        print((" "*q)+"*"*i)
+        q-=1
---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+i = 3
+j = 3
+
+for x in range(i):
+    for y in range(j):
+        
